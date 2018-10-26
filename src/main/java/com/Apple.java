2af90862afb42720961a4f26d905b15c14b853fb
gaosheng_global <gaosheng_global@andy.com>
2018-10-26 14:10:48 +0800

    22222

--- src/main/java/com/Apple.java
+++ src/main/java/com/Apple.java
@@ -2,7 +2,7 @@ package com;
 
 public class Apple {
 	public static void main(String[] args) {
-		System.out.println("----------------");
+		System.out.println("Number----------------");
 		System.out.println("String");
 		System.out.println("String");
 	}
