45d776a975bbccedeb078a9556b58db7c6c36d32
gaosheng_global <gaosheng_global@andy.com>
2018-10-26 14:03:12 +0800

    String

--- src/main/java/com/Apple.java
+++ src/main/java/com/Apple.java
@@ -3,5 +3,7 @@ package com;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("----------------");
+		System.out.println("String");
+		System.out.println("String");
 	}
 }
